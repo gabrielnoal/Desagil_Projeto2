d85e3eb8b4d581dd7b7bd592f2c7e4ef679eb817
Guirim <gui.aliperti9@gmail.com>
2018-03-30 04:33:21 -0300

    portas finalizadas funcionando, feito por guilherme aliperti e gabriel moura

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/OrGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/OrGate.java
@@ -5,14 +5,24 @@ public class OrGate extends Gate {
 	private NandGate nand2 = new NandGate();
 	private NandGate nandout = new NandGate();
 
-
-	@Override
-	public void connect(int pinIndex, Emitter emitter) {
-		nand1.connect(pinIndex, emitter);
-		nand2.connect(pinIndex, nand1);
-		
+	public OrGate(){
 		nandout.connect(0, nand1);
 		nandout.connect(1, nand2);
+	}
+
+	public void connect(int pinIndex, Emitter emitter) {
+		if(pinIndex==0){
+			nand1.connect(0, emitter);
+			nand1.connect(1, emitter);
+		}
+		else if(pinIndex ==1){
+			nand2.connect(0, emitter);
+			nand2.connect(1, emitter);
+		}
+		else{
+		}
+		
+		
 	}
 	@Override
 	public boolean read() {
--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/XorGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/XorGate.java
@@ -1,27 +1,33 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
-public class XorGate {
+public class XorGate extends Gate{
 	private NandGate nand1 = new NandGate();
 	private NandGate nand2 = new NandGate();
 	private NandGate nand3 = new NandGate();
 	private NandGate nand4 = new NandGate();
 	
-	
-	public void connect(int pinIndex, Emitter emitter) {
-		nand1.connect(pinIndex, emitter);
-		nand2.connect(pinIndex, emitter);
-		
-		
-		//nand2.connect(0, emitter);
-		//nand2.connect(1, nand1);
-		
-		nand3.connect(0, nand1);
-		nand3.connect(1, emitter);
-		
+	public XorGate(){
+		nand2.connect(1, nand1);
+		nand3.connect(1,nand1);
 		nand4.connect(0, nand2);
-		nand4.connect(1, nand3);
+		nand4.connect(1,nand3);
 	}
 	
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		nand1.connect(pinIndex, emitter);
+		
+		if(pinIndex==0){
+			nand2.connect(0,emitter);
+		}
+		else if (pinIndex==1){
+			nand3.connect(0,emitter);
+		}
+		else{
+			
+		}
+	}
+	@Override 
 	public boolean read(){
 		return nand4.read();
 	}
