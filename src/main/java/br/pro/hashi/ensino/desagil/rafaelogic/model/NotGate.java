78d2cee242085b76fd6bfe4f4a1c48c5513b5368
Guirim <gui.aliperti9@gmail.com>
2018-04-17 02:09:50 -0300

    desbugado

---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
@@ -0,0 +1,21 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+public class NotGate extends Gate {
+	public NotGate() {
+		super(1, "NotGate");
+	}
+
+	// Importa e cria uma variavel do tipo NandGate chanamemada nand
+	private NandGate nand = new NandGate();
+	
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		nand.connect(0, emitter);
+		nand.connect(1, emitter);
+	}
+
+	@Override
+	public boolean read() {
+		return nand.read();
+	}
+}
